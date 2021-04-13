60f1fc83199c3fa1e0735422971201d3bd91ea2a
Meghna-U <82393429+Meghna-U@users.noreply.github.com>
2021-04-13 22:33:03 +0530

    Create conditionalstatements1.c
    Used to calculate grade based on marks.

---
+++ conditionalstatements1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+   int m;
+   char g;
+   printf("Enter marks:");
+   scanf("%d",&m);
+   if(m>=85 && m<=100)
+   {
+     g='A';
+   }
+   else if(m>=70 && m<=84)
+   {
+     g='B';
+   }
+   else if(m>=55 && m<=69)
+   {
+     g='C';
+   }
+   else if(m>=40 && m<=54)
+   {  
+     g='D';
+   }
+   else
+   {
+     g='F';
+   }
+   printf("Grade: %c",g);
+}
